aa613962ac16c647e3f649440b9076bc252e805a
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-05-20 11:14:44 -0300

    backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,25 +12,25 @@ export const api = axios.create({
 
 // Função para buscar os produtos
 async function fetchProducts() {
+    const productsBaseURL = "https://api-db-json-products.vercel.app/"; // Base URL específica para os produtos
     const products = [];
     for (let i = 1; i <= 15; i++) {
         try {
-            // Buscar o produto usando a baseURL original
-            const response = await api.get(`/products/${i}`);
+            // Buscar o produto usando a baseURL específica para os produtos
+            const response = await axios.get(`${productsBaseURL}products/${i}`);
             const product = response.data;
 
             // Agora que temos os dados básicos do produto, buscar a descrição em outra URL
             const descriptionResponse = await axios.get(`https://loading-tech.vercel.app/products/${i}`);
-            // https://loading-tech.vercel.app/products/1
             product.description = descriptionResponse.data; // Adicionar a descrição ao objeto do produto
 
             products.push(product);
         } catch (error) {
-            console.error(`Erro ao buscar o produto com ID ${i}:`);
+            console.error(`Erro ao buscar o produto com ID ${i}:` , error);
         }
     }
     return products;
 }
 
 // Exemplo de como usar a função fetchProducts
-fetchProducts();
+fetchProducts()
