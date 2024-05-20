77faaa21b1da0acba64737a12b7f4e2fce905e32
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-05-20 09:53:44 -0300

    backend and frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 // json-server --watch db.json
 
 export const api = axios.create({
-    baseURL: "https://api-db-json-products.vercel.app/products"
+    baseURL: "https://api-db-json-products.vercel.app/"
     
 })
